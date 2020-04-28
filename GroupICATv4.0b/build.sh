91511a5ce5667be03562fcf4dbd80fc46be181cc
calhounlab <33673667+calhounlab@users.noreply.github.com>
2020-04-28 16:09:29 -0400

    Add files via upload

---
+++ GroupICATv4.0b/build.sh
@@ -0,0 +1,8 @@
+echo "Building gica image"
+wget https://trendscenter.org/trends/software/gift/software/stand_alone/gica_linux_april_27_2020.zip;
+sleep 10;
+unzip gica_linux_april_27_2020.zip; 
+chmod +x ./groupica;
+rm gica_linux_april_27_2020.zip;
+sudo docker image build -t gica_app:v91 .;
+echo "Done"
