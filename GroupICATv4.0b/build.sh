133ee285856255f45aef9f99dd4b0a04dd4aaac9
calhounlab <33673667+calhounlab@users.noreply.github.com>
2020-12-30 15:45:47 -0500

    Update build.sh

--- GroupICATv4.0b/build.sh
+++ GroupICATv4.0b/build.sh
@@ -1,8 +1,8 @@
 echo "Building gica image"
 wget https://trendscenter.org/trends/software/gift/software/stand_alone/gica_linux_nov_1_2020.zip;
 sleep 10;
-unzip gica_linux_may_1_2020.zip; 
+unzip gica_linux_nov_1_2020.zip; 
 chmod +x ./groupica;
-rm gica_linux_may_1_2020.zip;
+rm gica_linux_nov_1_2020.zip;
 sudo docker image build -t gica_app:v91 .;
 echo "Done"
